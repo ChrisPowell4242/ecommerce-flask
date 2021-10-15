b14c62f5fdf091c12ca1d34037b6823fadbd3c1c
Jerome <jerome0caruso@gmail.com>
2021-10-15 12:36:01 -0500

    Start

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-print("hello")
+from app import app, db
+from app.models import User, Post
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
